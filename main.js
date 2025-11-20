3fe5ad10949e8c5bb29dd5f56c6920d28135ba88
besukal gobena <besukalgobena@gmail.com>
2025-11-20 18:20:25 +0300

    adding skills, works & contact section

--- main.js
+++ main.js
@@ -48,3 +48,26 @@ $(document).ready(function() {
     });
   });
 });
+
+// Get modal elements
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
+const closeBtn = document.querySelector(".close");
+
+document.querySelectorAll(".parent img").forEach(img => {
+  img.addEventListener("click", () => {
+    modal.style.display = "flex";
+    modalImg.src = img.src;
+  });
+});
+
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
